8454d9b65a87e5652490b0a118d7842ecba057e3
HarshSen-TSM <harsh.sen@stackmentalist.com>
2026-05-06 15:33:36 +0530

    deleted the hello module and also added the individual templates

--- hello/ml/logger/__init__.py
+++
@@ -1,5 +0,0 @@
-# logger/__init__.py
-from hello.ml.logger.custom_logger import CustomLogger
-
-# Create a single shared logger instance
-GLOBAL_LOGGER = CustomLogger().get_logger("multi_agent_workflow")
--- hello/utils/ppt_helpers_utils/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services Module
-Contains presentation generation and JSON processing services
-"""
-
-__all__ = [
-    "frontend_json_processor",
-    "presentation_generator",
-]
